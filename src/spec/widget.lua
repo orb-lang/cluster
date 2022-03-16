689d6a589bf69ca2c8bae9dd5573f17e3a287006
Sam Atman <atmanistan@gmail.com>
2022-03-16 15:22:04 -0400

    Begin widget spec
    At last, bridge is put to its intended purpose: the construction
    and manipulation of widgets. Alhamdullilah.

---
+++ src/spec/widget.lua
@@ -0,0 +1,61 @@
+
+
+
+
+
+
+
+
+
+local core = require "qor:core"
+local cluster = require "cluster:cluster"
+local s = require "status:status" ()
+
+
+
+local new, Widget, Widget_M = cluster.genus()
+
+
+
+
+
+Widget.color = 'black'
+Widget.number = 1
+
+
+
+
+
+cluster.construct(new,
+   function(_, widget, color, number)
+      widget.color = color
+      widget.number = number
+      return widget
+   end)
+
+
+
+
+
+function Widget.report(widget)
+   s:chat("I'm a %s widget! number %d! hello!", widget.color, widget.number)
+end
+
+
+
+
+
+
+
+
+
+function Widget_M.__add(l_widget, r_widget)
+   return new(l_widget.color, l_widget.number + r_widget.number)
+end
+
+
+
+
+
+return new
+
